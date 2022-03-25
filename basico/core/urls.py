bea257c1276ea97c60a934b15dd2655ede55ea46
Erick A Fernandes <96123177+ErickFernan@users.noreply.github.com>
2022-03-25 11:29:35 -0300

    Aula 30

--- basico/core/urls.py
+++ basico/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, contato  # O ponto é do módulo core
+from .views import index, contato, produto  # O ponto é do módulo core
 
 urlpatterns = [
-    path('', index),  # Se for na raiz mando pro index
-    path('contato', contato),  # Se for no contato manda pro contato
+    path('', index, name='index'),  # Se for na raiz mando pro index
+    path('contato', contato, name='contato'),  # Se for no contato manda pro contato
+    path('produto/<int:pk>', produto, name='xuxa'),
 ]
--- basico/core/views.py
+++ basico/core/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from .models import Produto, Cliente
 
 
 def index(request):
-    context ={
+    produtos = Produto.objects.all()
+    context = {
         'curso': 'Programação Web com Django Framework',
-        'outro': 'Django é massa'
+        'outro': 'Django é massa',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    clientes = Cliente.objects.all()
+    context = {
+        'clientes': clientes
+    }
+    return render(request, 'contato.html', context)
+
+
+def produto(request, pk):
+    #  print(f'PK: {pk}')
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
