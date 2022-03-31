c41adb37f40b43013e094ca599eeb67e85e9a6bd
Erick A Fernandes <96123177+ErickFernan@users.noreply.github.com>
2022-03-31 17:11:03 -0300

    Aula 45

--- intermediario/core/views.py
+++ intermediario/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .forms import ContatoForm, ProdutoModelForm  # Estou importando o formulario criado no forms
 from django.contrib import messages
+from .models import Produto
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def produto(request):
