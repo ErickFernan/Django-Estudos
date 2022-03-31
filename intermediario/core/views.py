759f98415c3b67b19b7b8a4783a789860683ce1e
Erick A Fernandes <96123177+ErickFernan@users.noreply.github.com>
2022-03-31 15:04:10 -0300

    Aula 42

--- intermediario/core/views.py
+++ intermediario/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import ContatoForm  # Estou importando o formulario criado no forms
+from .forms import ContatoForm, ProdutoModelForm  # Estou importando o formulario criado no forms
 from django.contrib import messages
 
 
@@ -8,7 +8,27 @@ def index(request):
 
 
 def produto(request):
-    return render(request, 'produto.html')
+
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+
+            print(f'Nome: {prod.nome}')
+            print(f'Preço: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Imagem: {prod.imagem}')
+
+            messages.success(request, 'Produto salvo com sucesso.')
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, 'Erro ao salvar produto')
+    else:
+        form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'produto.html', context)
 
 
 def contato(request):
@@ -16,11 +36,10 @@ def contato(request):
 
     if str(request.method) == 'POST':
         if form.is_valid():
-            nome = form.cleaned_data['nome']
-            email = form.cleaned_data['email']
-            assunto = form.cleaned_data['assunto']
-            mensagem = form.cleaned_data['mensagem']
-
+            # nome = form.cleaned_data['nome']
+            # email = form.cleaned_data['email']
+            # assunto = form.cleaned_data['assunto']
+            # mensagem = form.cleaned_data['mensagem']
             # print('Mensagem Enviada')
             # print(f'Nome: {nome}')
             # print(f'E-mail: {email}')
