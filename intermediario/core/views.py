8759f4bcc6405fbe3f80c4469da1d06f2c5f653f
Erick A Fernandes <96123177+ErickFernan@users.noreply.github.com>
2022-03-31 16:32:01 -0300

    Aula 43, salvando dados no banco de dados

--- intermediario/core/views.py
+++ intermediario/core/views.py
@@ -12,12 +12,8 @@ def produto(request):
     if str(request.method) == 'POST':
         form = ProdutoModelForm(request.POST, request.FILES)
         if form.is_valid():
-            prod = form.save(commit=False)
 
-            print(f'Nome: {prod.nome}')
-            print(f'Preço: {prod.preco}')
-            print(f'Estoque: {prod.estoque}')
-            print(f'Imagem: {prod.imagem}')
+            form.save()
 
             messages.success(request, 'Produto salvo com sucesso.')
             form = ProdutoModelForm()
@@ -36,15 +32,6 @@ def contato(request):
 
     if str(request.method) == 'POST':
         if form.is_valid():
-            # nome = form.cleaned_data['nome']
-            # email = form.cleaned_data['email']
-            # assunto = form.cleaned_data['assunto']
-            # mensagem = form.cleaned_data['mensagem']
-            # print('Mensagem Enviada')
-            # print(f'Nome: {nome}')
-            # print(f'E-mail: {email}')
-            # print(f'Assunto: {assunto}')
-            # print(f'Mensagem: {mensagem}')
 
             form.send_mail()
 
