517a1498aa643b30dbe5aea7c5ea1a3e0f90298b
Erick A Fernandes <96123177+ErickFernan@users.noreply.github.com>
2022-04-12 11:30:52 -0300

    index do template reconfigurado para o django

--- avancado/fusion/settings.py
+++ avancado/fusion/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # mudei para postgres
         'NAME': 'fusion',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
